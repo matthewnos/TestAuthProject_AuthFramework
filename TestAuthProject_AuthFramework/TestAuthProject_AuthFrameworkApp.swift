43de4f97dff3337ed8047a96958590c0bc1bf561
Andrew F <afairchild@cloudingenuity.com>
2023-06-26 09:07:07 -0500

    Initial Commit

---
+++ TestAuthProject_AuthFramework/TestAuthProject_AuthFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAuthProject_AuthFrameworkApp.swift
+//  TestAuthProject_AuthFramework
+//
+//  Created by Andrew Fairchild on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TestAuthProject_AuthFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
